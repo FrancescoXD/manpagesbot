786f1010c0e360970ebcca0f9595fb130bd570ad
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2023-07-18 22:09:51 +0200

    v0.1.0 uploaded

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # manpagesbot
 A Telegram bot written in C
+
+## Requirements
+- libcurl
+- json-c
+- meson
+- ninja
+- cmake & make
+
+## How to compile
+First of all you need to clone the repository:
+```bash
+$ git clone https://github.com/FrancescoXD/manpagesbot.git
+$ cd manpagesbot
+```
+
+Now download the telebot library and compile it:
+```bash
+$ git clone https://github.com/smartnode/telebot.git
+$ cd telebot
+$ mkdir Build && cd build
+$ cmake ../
+$ make
+```
+
+After compiled the library, go to the main folder and do the same to build the bot yourself:
+```bash
+$ meson setup build
+$ cd build
+$ ninja
+```
+It will produce `manpagesbot`, which is the executable of the bot. To start it create a `.token` file with the token inside (get it from botfather on telegram!).
---
+++ src/manpagesbot.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <telebot.h>
+
+#define MSG_SIZE 1024
+#define TOKEN_SIZE 1024
+#define SIZE_OF_ARRAY(array) (sizeof(array) / sizeof(array[0]))
+
+int main(void) {
+	fprintf(stdout, "[info] Starting Telegram Bot...\n");
+
+	FILE *token_file = fopen(".token", "r");
+	if (token_file == NULL) {
+		perror("[error] .token file");
+		exit(EXIT_FAILURE);
+	}
+
+	char token[TOKEN_SIZE];
+	fscanf(token_file, "%s", token);
+	fprintf(stdout, "[info] Token: %s\n", token);
+	fclose(token_file);
+
+	telebot_handler_t handler;
+	if (telebot_create(&handler, token) != TELEBOT_ERROR_NONE) {
+		fprintf(stderr, "[error] Failed creating bot\n");
+		exit(EXIT_FAILURE);
+	}
+
+	telebot_user_t me;
+	if (telebot_get_me(handler, &me) != TELEBOT_ERROR_NONE) {
+		fprintf(stderr, "[error] Failed to get telegram bot information\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(stdout, "ID: %d\n", me.id);
+	fprintf(stdout, "Firstname: %s\n", me.first_name);
+	fprintf(stdout, "Username: %s\n", me.username);
+
+	telebot_put_me(&me);
+
+	int index;
+	int count;
+	int offset = -1;
+	telebot_error_e ret;
+	telebot_message_t message;
+	telebot_update_type_e update_types[] = {TELEBOT_UPDATE_TYPE_MESSAGE};
+
+	while (1) {
+		telebot_update_t *updates;
+		ret = telebot_get_updates(handler, offset, 20, 0, update_types, 0, &updates, &count);
+		if (ret != TELEBOT_ERROR_NONE) {
+			continue;
+		}
+
+		for (index = 0; index < count; ++index) {
+			message = updates[index].message;
+			if (message.text) {
+				char str[MSG_SIZE];
+				if (strstr(message.text, "/start")) {
+					snprintf(str, MSG_SIZE, "Hello @%s!", message.from->username);
+					ret = telebot_send_message(handler, message.chat->id, str, "HTML", 1, 0, message.message_id, "");
+				} else {
+					snprintf(str, MSG_SIZE, "%s", message.text);
+					ret = telebot_send_message(handler, message.chat->id, str, "HTML", 1, 0, message.message_id, "");
+				}
+
+				if (ret != TELEBOT_ERROR_NONE) {
+					fprintf(stderr, "[error] Unable to send message!\n");
+				}
+			}
+			offset = updates[index].update_id + 1;
+		}
+		telebot_put_updates(updates, count);
+		sleep(1);
+	}
+
+	telebot_destroy(handler);
+
+	return 0;
+}
